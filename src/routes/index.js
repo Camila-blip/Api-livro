c5af18d080aa28b346c72e94a18e37fce0906618
Camila Moura <cmiranda@fastprobr.com>
2023-07-29 22:41:54 -0300

    fix: controller

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,5 +2,8 @@ import livrosRoutes from "./livros.routes";
 const BASE_URL = "/api";
 
 export default function routes(app) {
+    app.use("/",((res)=>{
+        res.status(200).send("Message: Api Livro");
+    }));
     app.use(`${BASE_URL}/livros`, livrosRoutes);
 }
